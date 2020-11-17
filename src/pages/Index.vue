34e85fbe7de1757bafd03dc9034f68e723049545
Rich Andrews <54338608+hello-world-its-rich@users.noreply.github.com>
2020-11-16 20:46:19 -0500

    v2 again
    forgot name

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -93,7 +93,7 @@
 
 <form class="w-full max-w-sm flex mb-20 items-center justify-center" name="richlandingpage" method="POST" data-netlify="true">
   <div class="flex items-center border-b border-teal-500 py-2 justify-center">
-    <input class="flex items-center appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Leave a message...." aria-label="Full name">
+    <input class="flex items-center appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Leave a message...." name="message" aria-label="Full name">
     <button class="flex-shrink-0 justify-center bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
       📤 Send
     </button>
